7caf068aaa2a143184afd07b7e44bfa239c96f43
DHobby <cultof7@gmail.com>
2018-07-29 16:42:53 -0700

    trying again

---
+++ captcha.php
@@ -0,0 +1,16 @@
+<?php>
+<script type="text/javascript">
+    var a = Math.ceil(Math.random() * 10);
+    var b = Math.ceil(Math.random() * 10);
+    var c = a + b;
+
+    function DrawBotBoot() {
+        document.write("What is " + a + "+" + b + "?");
+        document.write("<input id='BotBootInput' type='text' maxlength='2' size='2'/>");
+    }
+    function ValidBotBoot() {
+        var d = document.getElementById('BotBootInput').value;
+        if (d == c) return true;
+        return false;
+    }
+</script>
